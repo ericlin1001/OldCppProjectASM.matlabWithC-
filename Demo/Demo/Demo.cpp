c8a4ed4e7b1e351dc1131bd8b67408aba339191e
ericln <nameljh1001@gmail.com>
2016-09-03 20:20:06 +0800

    first commit by script

---
+++ Demo/Demo/Demo.cpp
@@ -0,0 +1,15 @@
+// Demo.cpp : Defines the entry point for the console application.
+//
+
+
+#include<iostream>
+using namespace std;
+
+int main1()
+{
+	cout<<"aab"<<endl;
+	
+	system("pause");
+	return 0;
+}
+
